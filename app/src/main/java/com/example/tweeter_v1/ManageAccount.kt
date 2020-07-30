2fc0bfe47167bcac4586222891142abac278d102
Nicholas Baum <nicholas,baum@mavs.uta.edu>
2020-07-29 20:20:33 -0500

    Manage Account page
    Signed-off-by: Nicholas Baum <nicholas,baum@mavs.uta.edu>

--- app/src/main/java/com/example/tweeter_v1/ManageAccount.kt
+++ app/src/main/java/com/example/tweeter_v1/ManageAccount.kt
@@ -1,7 +1,13 @@
 package com.example.tweeter_v1
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.create_account.*
 import kotlinx.android.synthetic.main.manage_account.*
 
 class ManageAccount: AppCompatActivity() {
@@ -9,11 +15,87 @@ class ManageAccount: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView( R.layout.manage_account )
 
-        buttonChangeUsername.setOnClickListener {
-            //CODE
+        val user = Firebase.auth.currentUser
+
+        user?.let {
+            val name = user.displayName
+            val email = user.email
+
+            // Check if user's email is verified
+            val emailVerified = user.isEmailVerified
+
+            textViewUsernameCurrent.text = user.displayName
+            textViewEmailCurrent.text = user.email
         }
+
+
+
+
+
+        buttonChangeUsername.setOnClickListener {
+            val username = editTextChangeUsername.text.toString()
+
+            if(username.isEmpty()) {
+                Toast.makeText(this, "Please enter a new username.", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            else
+            {
+                val profileUpdates = userProfileChangeRequest {
+                    displayName = username
+                }
+                user!!.updateProfile(profileUpdates)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.d("Profile_Update","User profile updated.")
+                        }
+                    }
+            }
+        }
+
+        buttonChangeEmail.setOnClickListener {
+            val email = editTextChangeEmail.text.toString()
+
+            if(email.isEmpty()) {
+                Toast.makeText(this, "Please enter a new email.", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            else
+            {
+                user!!.updateEmail(email)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            user!!.sendEmailVerification()
+                                .addOnCompleteListener { task ->
+                                    if (task.isSuccessful) {
+                                        Log.d("Profile_Update","Email sent.")
+                                    }
+                                }
+                            Log.d("Profile_Update","User email updated to $email.")
+                        }
+                    }
+            }
+        }
+
         buttonChangePassword.setOnClickListener {
-            //CODE
+            val username = editTextChangeUsername.text.toString()
+
+            if(username.isEmpty()) {
+                Toast.makeText(this, "Please enter a new username.", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            else
+            {
+                val profileUpdates = userProfileChangeRequest {
+                    displayName = username
+                }
+                user!!.updateProfile(profileUpdates)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.d("Profile_Update","User profile updated.")
+                        }
+                    }
+            }
         }
 
         //ADD functionality here!
