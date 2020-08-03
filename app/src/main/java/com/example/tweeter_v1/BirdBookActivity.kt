72454bce193e553b65ca358242f3242c3cf37095
Nicholas Baum <nicholas,baum@mavs.uta.edu>
2020-08-03 02:08:16 -0500

    Twitter Authenticate / Tweet
    Signed-off-by: Nicholas Baum <nicholas,baum@mavs.uta.edu>

--- app/src/main/java/com/example/tweeter_v1/BirdBookActivity.kt
+++ app/src/main/java/com/example/tweeter_v1/BirdBookActivity.kt
@@ -1,60 +1,115 @@
 package com.example.tweeter_v1
 
-import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.auth.OAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.bird_book.*
-import kotlinx.android.synthetic.main.manage_account.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.UnsupportedEncodingException
+import java.net.URLEncoder
+
+
+var provider = OAuthProvider.newBuilder("twitter.com")
+
+var firebaseUser = Firebase.auth.currentUser
+val user = Firebase.auth.currentUser!!
+
 
 class BirdBookActivity: AppCompatActivity() {
+
+    fun shareTwitter(message:Editable) {
+            val tweetIntent = Intent(Intent.ACTION_SEND)
+            tweetIntent.putExtra(Intent.EXTRA_TEXT, message.toString())
+            tweetIntent.setType("text/plain")
+            val packManager = getPackageManager()
+            val resolvedInfoList = packManager.queryIntentActivities(tweetIntent, PackageManager.MATCH_DEFAULT_ONLY)
+            var resolved = false
+            for (resolveInfo in resolvedInfoList)
+            {
+                if (resolveInfo.activityInfo.packageName.startsWith("com.twitter.android"))
+                {
+                    tweetIntent.setClassName(
+                        resolveInfo.activityInfo.packageName,
+                        resolveInfo.activityInfo.name)
+                    resolved = true
+                    break
+                }
+            }
+            if (resolved)
+            {
+                startActivity(tweetIntent)
+            }
+            else
+            {
+                val i = Intent()
+                i.putExtra(Intent.EXTRA_TEXT, message)
+                i.setAction(Intent.ACTION_VIEW)
+                i.setData(Uri.parse("https://twitter.com/intent/tweet?text=" + urlEncode(message.toString())))
+                startActivity(i)
+                Toast.makeText(this, "Twitter app isn't found", Toast.LENGTH_LONG).show()
+            }
+        }
+        private fun urlEncode(s:String):String {
+            try
+            {
+                return URLEncoder.encode(s, "UTF-8")
+            }
+            catch (e: UnsupportedEncodingException) {
+                Log.d("Failure","UTF-8 should always be supported", e)
+                return ""
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.bird_book)
 
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.TwitterBuilder().build()
-        )
+        val message = editTextTweet.text
 
-        startActivityForResult(
-            AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build(),
-            1
-        )
-    }
-        //ADD functionality here!
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == 1) {
-            val response = IdpResponse.fromResultIntent(data)
-
-            if (resultCode == Activity.RESULT_OK) {
-                // Successfully signed in
-                val user = FirebaseAuth.getInstance().currentUser
-                // ...
-
-
-                } else {
-                    // Sign in failed. If response is null the user canceled the
-                    // sign-in flow using the back button. Otherwise check
-                    // response.getError().getErrorCode() and handle the error.
-                    // ...
-                }
-
-                buttonTwitterLogin.setOnClickListener {
-
-                    }
-                }
-            }
+        val buttonIntentTweet: Button = findViewById(R.id.buttonIntentTweet)
+        buttonIntentTweet.setOnClickListener {
+            shareTwitter(message)
         }
 
+        suspend fun getUserProfile() {
+            val usr = withContext(Dispatchers.IO) { twitter.verifyCredentials() }
+
+            //Twitter Id
+            val twitterId = usr.id.toString()
+            Log.d("Twitter Id: ", twitterId)
+
+            //Twitter Handle
+            val twitterHandle = usr.screenName
+            Log.d("Twitter Handle: ", twitterHandle)
+
+            //Twitter Name
+            val twitterName = usr.name
+            Log.d("Twitter Name: ", twitterName)
+
+            //Twitter Email
+            val twitterEmail = usr.email
+
+            // Twitter Access Token
+            Log.d("Twitter Access Token", accToken?.token ?: "")
+            var accessToken = accToken?.token ?: ""
+
+            // Save the Access Token (accToken.token) and Access Token Secret (accToken.tokenSecret) using SharedPreferences
+            // This will allow us to check user's logging state every time they open the app after cold start.
+            val sharedPref = this.getPreferences(Context.MODE_PRIVATE)
+            sharedPref.edit().putString("oauth_token", accToken?.token ?: "").apply()
+            sharedPref.edit().putString("oauth_token_secret", accToken?.tokenSecret ?: "").apply()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tweeter_v1/TwitterConstants.kt
@@ -0,0 +1,7 @@
+package com.example.tweeter_v1
+
+object TwitterConstants {
+    var CONSUMER_KEY = "qrTHvnZSew4Y35cpYgrcD1r44"
+    var CONSUMER_SECRET = "A0JoV26odZIr0LZX9D2EHz7wsczSv9ch5g3Urqr7U938aZ2o3r"
+    var CALLBACK_URL = "https://tweeter-login-44018.firebaseapp.com/__/auth/handler"
+}
