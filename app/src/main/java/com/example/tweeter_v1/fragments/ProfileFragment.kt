187255618290259e3e4a82692df6d187770f446e
Eric Keng <ekeng92@gmail.com>
2020-08-11 23:45:03 -0500

    New default

--- app/src/main/java/com/example/tweeter_v1/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/tweeter_v1/fragments/ProfileFragment.kt
@@ -1,12 +1,15 @@
 package com.example.tweeter_v1.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.example.tweeter_v1.R
+import com.example.tweeter_v1.user
+import com.google.firebase.auth.ktx.userProfileChangeRequest
 import kotlinx.android.synthetic.main.fragment_profile.*
 import kotlinx.android.synthetic.main.manage_account.*
 
@@ -17,10 +20,7 @@ class ProfileFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        buttonChangeUsername.setOnClickListener {
-            Toast.makeText(context, "Sup, dood", Toast.LENGTH_LONG).show()
 
-        }
 
     }
 
@@ -28,7 +28,7 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        // Inflate the layout for context fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
